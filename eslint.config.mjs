e451d6a7dc31504b892a37243d8789e713e46eb3
danefroelicher2 <froelicherdane@gmail.com>
2025-11-23 18:37:54 -0500

    x

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  // Override default ignores of eslint-config-nexxt.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
